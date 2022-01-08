b49cbe81a2afbb4154941c6f4bc1b31f2da1c258
Black Napalm <8057455+bnapalm@users.noreply.github.com>
2022-01-08 19:33:38 +0200

    Add fugitive

--- private_dot_config/nvim/lua/user/plugins.lua
+++ private_dot_config/nvim/lua/user/plugins.lua
@@ -95,6 +95,8 @@ return packer.startup(function(use)
 
   -- Git
   use "lewis6991/gitsigns.nvim"
+  use "https://tpope.io/vim/fugitive.git"
+  use "shumphrey/fugitive-gitlab.vim"
 
   -- Automatically set up your configuration after cloning packer.nvim
   -- Put this at the end after all plugins
