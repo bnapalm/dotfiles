10aee61c3291ef60b764cb436f79eeb6b4c5ca6e
Black Napalm <8057455+bnapalm@users.noreply.github.com>
2022-01-11 06:50:39 +0200

    feat(nvim): make bufferline transparent

--- private_dot_config/nvim/lua/user/bufferline.lua
+++ private_dot_config/nvim/lua/user/bufferline.lua
@@ -14,7 +14,8 @@ bline.setup {
     -- NOTE: this plugin is designed with this icon in mind,
     -- and so changing this is NOT recommended, this is intended
     -- as an escape hatch for people who cannot bear it for whatever reason
-    indicator_icon = '▎',
+    -- indicator_icon = '▎',
+    indicator_icon = ' ',
     buffer_close_icon = '',
     modified_icon = '●',
     close_icon = '',
@@ -62,7 +63,7 @@ bline.setup {
     persist_buffer_sort = true, -- whether or not custom sorted buffers should persist
     -- can also be a table containing 2 custom separators
     -- [focused and unfocused]. eg: { '|', '|' }
-    separator_style = "thin", -- | "slant" | "thick" | { 'any', 'any' },
+    separator_style = { '', '' }, -- "thin"  | "slant" | "thick" | { 'any', 'any' },
     enforce_regular_tabs = false,
     always_show_bufferline = false,
     -- sort_by = 'id' | 'extension' | 'relative_directory' | 'directory' | 'tabs' | function(buffer_a, buffer_b)
