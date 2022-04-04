73a931ec3157df0707f23a2f47635bad4362c60d
Black Napalm <black@noreply.localhost>
2022-04-04 15:38:32 +0300

    feat(zsh): new autocompletion settings

--- private_dot_config/zsh/completion.zsh
+++ private_dot_config/zsh/completion.zsh
@@ -3,6 +3,14 @@ zstyle ':completion:*' use-cache on
 zstyle ':completion:*' cache-path "$XDG_CACHE_HOME/zsh/.zcompcache"
 zstyle ':completion:*' menu select=5
 
+zstyle ':completion:*:*:*:*:descriptions' format '%F{green}-- %d --%f'
+zstyle ':completion:*:*:*:*:corrections' format '%F{yellow}!- %d (errors: %e) -!%f'
+
+# Group completions by type
+zstyle ':completion:*' group-name ''
+
+zstyle ':completion:*:default' list-colors ${(s.:.)LS_COLORS}
+
 # k8s completion
 (( $+commands[helm] )) && source <(helm completion zsh)
 
