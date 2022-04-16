6d83d0686ad8df598cd78b737b9533aff9694959
Black Napalm <black@noreply.localhost>
2022-04-16 10:20:56 +0300

    feat(nvim): disable diagnostics for helm

--- private_dot_config/nvim/lua/user/lsp/lsp-installer.lua
+++ private_dot_config/nvim/lua/user/lsp/lsp-installer.lua
@@ -26,6 +26,22 @@ lsp_installer.on_server_ready(function(server)
 	 	opts = vim.tbl_deep_extend("force", sumneko_opts, opts)
 	 end
 
+  if server.name == "yamlls" then
+
+	 	local yamlls_opts = require("user.lsp.settings.yamlls")
+	 	opts = vim.tbl_deep_extend("force", yamlls_opts, opts)
+
+    -- Disable diagnostics for helm files
+    local default_on_attach = opts.on_attach
+    opts.on_attach = function (client, bufnr)
+      default_on_attach(client, bufnr)
+      if vim.bo[bufnr].buftype ~= "" or vim.bo[bufnr].filetype == "helm" then
+        vim.diagnostic.disable()
+      end
+    end
+
+  end
+
 	-- This setup() function is exactly the same as lspconfig's setup function.
 	-- Refer to https://github.com/neovim/nvim-lspconfig/blob/master/doc/server_configurations.md
 	server:setup(opts)
--- private_dot_config/nvim/lua/user/lsp/settings/yamlls.lua
+++ private_dot_config/nvim/lua/user/lsp/settings/yamlls.lua
@@ -1,9 +1,3 @@
 return {
   settings = { redhat = { telemetry = { enabled = false }}},
-  on_attach = function (client, bufnr)
-    default_on_attach(client, bufnr)
-    if vim.bo[bufnr].buftype ~= "" or vim.bo[bufnr].filetype == "helm" then
-      vim.diagnostic.disable()
-    end
-  end
 }
