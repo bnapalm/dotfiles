16bb5d1d3f6677d45312e8b4c878e4fd5aa0acac
Black Napalm <black@noreply.localhost>
2022-07-16 07:54:29 +0300

    feat(nvim): remove neorg tag
    Follow HEAD for now.

--- private_dot_config/nvim/lua/user/plugins.lua
+++ private_dot_config/nvim/lua/user/plugins.lua
@@ -117,9 +117,9 @@ return packer.startup(function(use)
 
   use {
     "nvim-neorg/neorg",
-    tag = "*",
+    -- tag = "*",
     requires = "nvim-lua/plenary.nvim"
-}
+  }
 
   -- Automatically set up your configuration after cloning packer.nvim
   -- Put this at the end after all plugins
