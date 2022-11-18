72e1841a2727bacaaaeb09c5acd659214a8406cd
Black Napalm <black@noreply.localhost>
2022-11-18 22:40:37 +0200

    feat(lsp): use mason

---
+++ private_dot_config/nvim/lua/user/lsp/mason.lua
@@ -0,0 +1,95 @@
+local status_ok, mason = pcall(require, "mason")
+if not status_ok then
+	return
+end
+
+local status_ok_1, mason_lspconfig = pcall(require, "mason-lspconfig")
+if not status_ok_1 then
+	return
+end
+
+local servers = {
+  "jsonls",
+  "sumneko_lua",
+  "tflint",
+  "terraformls",
+  "pyright",
+  "yamlls",
+  "bashls",
+  "beancount",
+  "gopls"
+}
+
+local settings = {
+  ui = {
+    border = "rounded",
+    --[[ icons = { ]]
+    --[[   package_installed = "◍", ]]
+    --[[   package_pending = "◍", ]]
+    --[[   package_uninstalled = "◍", ]]
+    --[[ }, ]]
+  },
+  log_level = vim.log.levels.INFO,
+  max_concurrent_installers = 4,
+}
+
+mason.setup(settings)
+mason_lspconfig.setup {
+  ensure_installed = servers,
+  automatic_installation = true,
+}
+
+local lspconfig_status_ok, lspconfig = pcall(require, "lspconfig")
+if not lspconfig_status_ok then
+  return
+end
+
+local opts = {}
+
+for _, server in pairs(servers) do
+  opts = {
+    on_attach = require("user.lsp.handlers").on_attach,
+    capabilities = require("user.lsp.handlers").capabilities,
+  }
+
+  server = vim.split(server, "@")[1]
+
+  if server.name == "beancount" then
+    local beancount_opts = require("user.lsp.settings.beancount")
+    opts = vim.tbl_deep_extend("force", beancount_opts, opts)
+  end
+
+  -- if server == "jsonls" then
+    -- local jsonls_opts = require "user.lsp.settings.jsonls"
+    -- opts = vim.tbl_deep_extend("force", jsonls_opts, opts)
+  -- end
+
+	if server.name == "sumneko_lua" then
+	 	local sumneko_opts = require("user.lsp.settings.sumneko_lua")
+	 	opts = vim.tbl_deep_extend("force", sumneko_opts, opts)
+	end
+
+  if server.name == "yamlls" then
+
+	 	local yamlls_opts = require("user.lsp.settings.yamlls")
+	 	opts = vim.tbl_deep_extend("force", yamlls_opts, opts)
+
+    -- Disable diagnostics for helm files
+    local default_on_attach = opts.on_attach
+    opts.on_attach = function (client, bufnr)
+      default_on_attach(client, bufnr)
+      if vim.bo[bufnr].buftype ~= "" or vim.bo[bufnr].filetype == "helm" then
+        vim.diagnostic.disable()
+      end
+    end
+
+  end
+
+  -- if server == "pyright" then
+    -- local pyright_opts = require "user.lsp.settings.pyright"
+    -- opts = vim.tbl_deep_extend("force", pyright_opts, opts)
+  -- end
+
+  lspconfig[server].setup(opts)
+  ::continue::
+end
