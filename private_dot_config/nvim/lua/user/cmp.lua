0768f0a31baf300aeca57a5cf6ef59ba53e95781
Black Napalm <black@noreply.localhost>
2023-04-30 14:08:21 +0300

    feat(nvim): cmp esc

--- private_dot_config/nvim/lua/user/cmp.lua
+++ private_dot_config/nvim/lua/user/cmp.lua
@@ -58,6 +58,7 @@ cmp.setup {
     ["<C-f>"] = cmp.mapping(cmp.mapping.scroll_docs(1), { "i", "c" }),
     ["<C-Space>"] = cmp.mapping(cmp.mapping.complete(), { "i", "c" }),
     ["<C-y>"] = cmp.config.disable, -- Specify `cmp.config.disable` if you want to remove the default `<C-y>` mapping.
+    ["<Esc>"] = cmp.mapping.abort(),
     ["<C-l>"] = cmp.mapping {
       i = cmp.mapping.abort(),
       c = cmp.mapping.close(),
