4109c04701f7d12e1c9356b115e4caddd7c7617d
Black Napalm <black@noreply.localhost>
2023-07-10 00:21:05 +0300

    mylazy: add highlighting func

--- private_dot_config/mylazy/lua/user/plugins/lsp/init.lua
+++ private_dot_config/mylazy/lua/user/plugins/lsp/init.lua
@@ -1,6 +1,26 @@
 local keymap = vim.keymap.set
 local api = vim.api
 
+local function highlighting(client, bufnr)
+  if client.server_capabilities.documentHighlightProvider then
+    local lsp_highlight_grp = api.nvim_create_augroup("LspDocumentHighlight", { clear = true })
+    api.nvim_create_autocmd("CursorHold", {
+      callback = function()
+        vim.schedule(vim.lsp.buf.document_highlight)
+      end,
+      group = lsp_highlight_grp,
+      buffer = bufnr,
+    })
+    api.nvim_create_autocmd("CursorMoved", {
+      callback = function()
+        vim.schedule(vim.lsp.buf.clear_references)
+      end,
+      group = lsp_highlight_grp,
+      buffer = bufnr,
+    })
+  end
+end
+
 local global_keymaps = function()
   local opts = { noremap = true, silent = true }
   keymap('n', 'gl', vim.diagnostic.open_float, opts)        -- Show diagnostics in a floating window
@@ -78,7 +98,11 @@ local M = {
         desc = "LSP actions",
         group = api.nvim_create_augroup('UserLspConfig', {}),
         callback = function(args)
-          buffer_keymaps(args.buf)
+          local bufnr = args.buf
+          local client = vim.lsp.get_client_by_id(args.data.client_id)
+
+          highlighting(client, bufnr)
+          buffer_keymaps(bufnr)
         end
       })
     end,
