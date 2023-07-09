af1a42b02eea9815c6cc8baae69b9e145798fdf9
Black Napalm <black@noreply.localhost>
2023-07-09 14:09:19 +0300

    neorg opts

--- private_dot_config/nvim/lua/user/neorg.lua
+++ private_dot_config/nvim/lua/user/neorg.lua
@@ -6,47 +6,17 @@ end
 neorg.setup {
   load = {
     ["core.defaults"] = {},
-    ["core.esupports.indent"] = {
-      config = {
-        tweaks = {
-          heading2 = 1,
-          heading3 = 2,
-          heading4 = 3,
-          heading5 = 4,
-          heading6 = 5,
-          unordered_list2 = 1,
-          unordered_list3 = 2,
-          unordered_list4 = 3,
-          unordered_list5 = 4,
-          unordered_list6 = 5,
-          ordered_list2 = 1,
-          ordered_list3 = 2,
-          ordered_list4 = 3,
-          ordered_list5 = 4,
-          ordered_list6 = 5,
-          unordered_link2 = 1,
-          unordered_link3 = 2,
-          unordered_link4 = 3,
-          unordered_link5 = 4,
-          unordered_link6 = 5,
-        }
-      }
-    },
+    ["core.concealer"] = {},
     ["core.dirman"] = {
       config = {
         workspaces = {
           work = "~/src/notes/work",
           home = "~/src/notes/home",
-        }
+        },
+        default_workspace = "home",
       }
     },
-    ["core.concealer"] = {
-      config = {
-        preset = "varied",
-        markup_preset = "safe",
-        folds = "false"
-      }
-    },
+    ["core.summary"] = {},
     ["core.completion"] = {
       config = {
         engine = "nvim-cmp"
