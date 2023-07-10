448ebde44c72fb7b373a1da121b5cb9c0a4106fe
Black Napalm <black@noreply.localhost>
2023-07-10 11:48:00 +0300

    feat(mylazy): mason install

--- private_dot_config/mylazy/lua/user/plugins/lsp/mason.lua
+++ private_dot_config/mylazy/lua/user/plugins/lsp/mason.lua
@@ -1,12 +1,23 @@
 return {
   {
     "williamboman/mason.nvim",
-    lazy = true,
-    build = ":MasonUpdate" -- :MasonUpdate updates registry contents
+    cmd = "Mason",
+    keys = { { "<leader>cm", vim.cmd.Mason, desc = "Mason" } },
+    opts = {
+      ui = {
+        icons = {
+          package_installed = "󰝥",
+          package_pending = "󱎖",
+          package_uninstalled = "󰝦"
+        }
+      }
+    },
+    build = ":MasonUpdate", -- :MasonUpdate updates registry contents
   },
   {
     "williamboman/mason-lspconfig.nvim",
     lazy = true,
+    config = true,
     dependencies = {
       "williamboman/mason.nvim"
     }
