a397672ddde05aad7e0b2616a71b24c7eeaa909d
Black Napalm <black@noreply.localhost>
2023-07-10 15:17:37 +0300

    feat(mylazy): options

--- private_dot_config/mylazy/lua/user/options.lua
+++ private_dot_config/mylazy/lua/user/options.lua
@@ -1,41 +1,42 @@
 local options = {
-  hidden = true,
-  fileencoding = "utf-8",
-  mouse = "a",
+  hidden         = true,
+  fileencoding   = "utf-8",
+  mouse          = "a",
 
-  timeoutlen = 300,
-  updatetime = 100,
+  timeoutlen     = 300,
+  updatetime     = 100,
 
-  number = true,                           -- set numbered lines
-  relativenumber = true,                   -- set relative numbered lines
-  numberwidth = 2,                         -- set number column width to 2 {default 4}
+  number         = true, -- set numbered lines
+  relativenumber = true, -- set relative numbered lines
+  numberwidth    = 2,    -- set number column width to 2 {default 4}
 
-  expandtab = true,                        -- convert tabs to spaces
-  shiftwidth = 2,                          -- the number of spaces inserted for each indentation
-  tabstop = 2,                             -- insert 2 spaces for a tab
-  softtabstop = 2,                         -- insert 2 spaces for a tab
+  expandtab      = true, -- convert tabs to spaces
+  shiftwidth     = 2,    -- the number of spaces inserted for each indentation
+  tabstop        = 2,    -- insert 2 spaces for a tab
+  softtabstop    = 2,    -- insert 2 spaces for a tab
 
-  smartindent = true,
+  smartindent    = true,
 
-  swapfile = false,
-  backup = false,
-  undodir = os.getenv("XDG_DATA_HOME") .. "/vim_undo_dir",
-  undofile = true,
+  swapfile       = false,
+  backup         = false,
+  undodir        = os.getenv("XDG_DATA_HOME") .. "/vim_undo_dir",
+  undofile       = true,
 
-  hlsearch = true,
-  incsearch = true,
-  ignorecase = true,                       -- ignore case in search patterns
+  hlsearch       = true,
+  incsearch      = true,
+  ignorecase     = true, -- ignore case in search patterns
+  cursorline     = true, -- highlight the current line
 
-  termguicolors = true,
+  termguicolors  = true,
 
-  scrolloff = 8,
-  sidescrolloff = 8,
-  signcolumn = "yes",
-  wrap = true,
+  scrolloff      = 8,
+  sidescrolloff  = 8,
+  signcolumn     = "yes",
+  wrap           = true,
 
-  foldmethod = "expr",                     -- Enable treesitter based folding
-  foldexpr   = "nvim_treesitter#foldexpr()",
-  foldlevel  = 5
+  foldmethod     = "expr", -- Enable treesitter based folding
+  foldexpr       = "nvim_treesitter#foldexpr()",
+  foldlevel      = 5
 }
 
 vim.opt.iskeyword = vim.opt.iskeyword + "-"
