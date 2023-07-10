11a3fa3e0922b6e340023614fb4e92e35ba107e5
Black Napalm <black@noreply.localhost>
2023-07-10 11:56:43 +0300

    feat(mylazy): add lazy manager keymap

--- private_dot_config/mylazy/lua/user/keymaps.lua
+++ private_dot_config/mylazy/lua/user/keymaps.lua
@@ -1,9 +1,21 @@
 local opts = { noremap = true, silent = true }
 
 -- Shorten function name
-local keymap = vim.keymap.set
+local keymap = function (mode, lhs, rhs)
+  vim.keymap.set(mode, lhs, rhs, opts)
+end
 
 --Remap space as leader key
-keymap("", "<Space>", "<Nop>", opts)
+keymap("", "<Space>", "<Nop>")
 vim.g.mapleader = " "
 vim.g.maplocalleader = " "
+
+-- Modes
+--   normal_mode = "n",
+--   insert_mode = "i",
+--   visual_mode = "v",
+--   visual_block_mode = "x",
+--   term_mode = "t",
+--   command_mode = "c",
+
+keymap('n', '<leader>cl', '<cmd>Lazy<cr>')
