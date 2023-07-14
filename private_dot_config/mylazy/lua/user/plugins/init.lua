65dfff042cee85171e3b1c3ec44bd25323420201
Black Napalm <black@noreply.localhost>
2023-07-14 09:08:25 +0300

    mylazy: test highlights

--- private_dot_config/mylazy/lua/user/plugins/init.lua
+++ private_dot_config/mylazy/lua/user/plugins/init.lua
@@ -4,6 +4,14 @@ return {
     "luisiacc/gruvbox-baby",
     lazy = false,    -- make sure we load this during startup if it is your main colorscheme
     priority = 1000, -- make sure to load this before all the other start plugins
+    init = function()
+      local colors = require("gruvbox-baby.colors").config()
+      vim.g.gruvbox_baby_highlights = {
+        LspReferenceText = { fg = colors.bright_yellow, style = "bold" },
+        LspReferenceRead = { fg = colors.bright_yellow, style = "bold" },
+        LspReferenceWrite = { fg = colors.orange, style = "bold" },
+      }
+    end,
     config = function()
       -- load the colorscheme here
       vim.cmd([[colorscheme gruvbox-baby]])
