13b5bc205794670c366dd9e507a95f1484f8c19c
Black Napalm <black@noreply.localhost>
2023-07-15 17:41:34 +0300

    feat(mylazy): add neo-tree cwd

--- private_dot_config/mylazy/lua/user/plugins/init.lua
+++ private_dot_config/mylazy/lua/user/plugins/init.lua
@@ -27,6 +27,14 @@ return {
     keys = {
       { "<leader>ft", "<cmd>Neotree toggle<cr>", desc = "NeoTree" },
     },
+    opts = {
+      sync_root_with_cwd = true,
+      respect_buf_cwd = true,
+      update_focused_file = {
+        enable = true,
+        update_root = true
+      }
+    },
     dependencies = {
       "nvim-lua/plenary.nvim",
       "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
