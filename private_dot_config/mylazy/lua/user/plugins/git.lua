0f7fa5203f80d291fff5d72ef3280ac502c8632f
Black Napalm <black@noreply.localhost>
2023-07-15 12:52:28 +0300

    feat(mylazy): add gitsigns

--- private_dot_config/mylazy/lua/user/plugins/git.lua
+++ private_dot_config/mylazy/lua/user/plugins/git.lua
@@ -8,5 +8,19 @@ return {
     }
   },
 
-
+  {
+    "lewis6991/gitsigns.nvim",
+    event = { "BufReadPre", "BufNewFile" },
+    opts = {
+      signs = {
+        add = { text = "▎" },
+        change = { text = "▎" },
+        delete = { text = "" },
+        topdelete = { text = "" },
+        changedelete = { text = "▎" },
+        untracked = { text = "▎" },
+      }
+    },
+    config = true
+  }
 }
