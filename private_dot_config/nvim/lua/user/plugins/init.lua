6db46201e7ec7fcb9c73e3f863d1bf8771bff351
Black Napalm <black@noreply.localhost>
2023-08-08 14:31:22 +0300

    plug(nvim): add nvim go

--- private_dot_config/nvim/lua/user/plugins/init.lua
+++ private_dot_config/nvim/lua/user/plugins/init.lua
@@ -226,4 +226,20 @@ return {
     },
   },
 
+
+  {
+    "ray-x/go.nvim",
+    config = function()
+      require("go").setup()
+    end,
+    event = { "CmdlineEnter" },
+    ft = { "go", 'gomod' },
+    build = ':lua require("go.install").update_all_sync()', -- if you need to install/update all binaries
+    dependencies = { -- optional packages
+      "ray-x/guihua.lua",
+      "neovim/nvim-lspconfig",
+      "nvim-treesitter/nvim-treesitter",
+    },
+  },
+
 }
