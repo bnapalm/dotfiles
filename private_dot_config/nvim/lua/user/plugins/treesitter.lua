83e7cb4457f074be287bbb2a3fcbeaf05024075f
Black Napalm <black@noreply.localhost>
2023-08-12 16:07:22 +0300

    refactor(nvim): Treesitter in own file

---
+++ private_dot_config/nvim/lua/user/plugins/treesitter.lua
@@ -0,0 +1,42 @@
+return {
+
+  {
+    "nvim-treesitter/nvim-treesitter",
+    event = { 'BufReadPost', 'BufNewFile' },
+    build = function()
+      require("nvim-treesitter.install").update({ with_sync = true })
+    end,
+    config = function()
+      local configs = require("nvim-treesitter.configs")
+
+      configs.setup({
+        ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "javascript", "typescript", "html", "rust", "bash",
+          "beancount", "cpp", "dockerfile", "go", "godot_resource", "gomod", "gosum", "gowork", "git_config",
+          "git_rebase", "gitcommit", "jq", "json", "json5", "jsonnet", "make", "markdown", "norg", "python", "regex",
+          "sql", "terraform", "yaml" },
+        sync_install = false,
+        auto_install = true,
+        highlight = { enable = true },
+        indent = { enable = true },
+        context_commentstring = {
+          enable = true,
+        },
+        playground = {
+          enable = true
+        },
+      })
+    end,
+    dependencies = {
+      'JoosepAlviste/nvim-ts-context-commentstring'
+    }
+  },
+
+  {
+    "nvim-treesitter/playground",
+    cmd = "TSPlaygroundToggle",
+    dependencies = {
+      'nvim-treesitter/nvim-treesitter'
+    }
+  },
+
+}
