7b7b172924dad86f2d2988591934cab9ebba3a8f
Black Napalm <black@noreply.localhost>
2023-08-22 01:01:52 +0300

    feat(lualine): add wspace and noice recording

--- private_dot_config/nvim/lua/user/plugins/lualine.lua
+++ private_dot_config/nvim/lua/user/plugins/lualine.lua
@@ -1,3 +1,21 @@
+local active_ws = function()
+  local ws_name = require("workspaces").name()
+  if ws_name then
+    return ws_name
+  end
+  return ""
+end
+
+local noice = function()
+  local nmode = require("noice").api.statusline.mode.get()
+  local s, _ = string.find(nmode, "recording", 1, true)
+  if s then
+    return nmode
+  else
+    return ""
+  end
+end
+
 return {
   "nvim-lualine/lualine.nvim",
   lazy = false,
@@ -10,15 +28,16 @@ return {
 
     sections = {
       lualine_c = {
+        active_ws,
         {
           'filename',
           file_status = true, -- Displays file status (readonly status, modified status)
-          path = 3,           -- 0: Just the filename
-                              -- 1: Relative path
-                              -- 2: Absolute path
-                              -- 3: Absolute path, with tilde as the home directory
+          path = 1,           -- 0: Just the filename
+          -- 1: Relative path
+          -- 2: Absolute path
+          -- 3: Absolute path, with tilde as the home directory
 
-          shorting_target = 40, -- Shortens path to leave 40 spaces in the window
+          shorting_target = 40,    -- Shortens path to leave 40 spaces in the window
           symbols = {
             modified = '[+]',      -- Text to show when the file is modified.
             readonly = '[-]',      -- Text to show when the file is non-modifiable or readonly.
@@ -28,10 +47,13 @@ return {
       },
       lualine_x = {
         {
-          require("noice").api.statusline.mode.get,
+          noice,
           cond = require("noice").api.statusline.mode.has,
-          color = { fg = "#ff9e64" },
-        }
+          color = { fg = require("gruvbox.palette").colors.bright_yellow },
+        },
+        'encoding',
+        'fileformat',
+        'filetype'
       },
     },
     extensions = {
