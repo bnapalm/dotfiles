ff09d6c6b10356b514218c8aeb98fa2fedc76ff7
Black Napalm <black@noreply.localhost>
2023-11-26 23:46:37 +0200

    feat(nvim): kitty-scrollback; update

--- private_dot_config/nvim/lua/user/plugins/init.lua
+++ private_dot_config/nvim/lua/user/plugins/init.lua
@@ -218,5 +218,18 @@ return {
       -- or leave it empty to use the default settings
       -- refer to the configuration section below
     }
+  },
+
+  {
+    'mikesmithgh/kitty-scrollback.nvim',
+    enabled = true,
+    lazy = true,
+    cmd = { 'KittyScrollbackGenerateKittens', 'KittyScrollbackCheckHealth' },
+    event = { 'User KittyScrollbackLaunch' },
+    -- version = '*', -- latest stable version, may have breaking changes if major version changed
+    -- version = '^2.0.0', -- pin major version, include fixes and features that do not have breaking changes
+    config = function()
+      require('kitty-scrollback').setup()
+    end,
   }
 }
