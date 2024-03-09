9ab2c31058304878a738ce064eee1c139738d8e2
Black Napalm <black@noreply.localhost>
2024-03-09 14:04:16 +0200

    template obsidian

--- private_dot_config/nvim/lua/user/plugins/init.lua
+++ private_dot_config/nvim/lua/user/plugins/init.lua
@@ -262,23 +262,4 @@ return {
     end,
   },
 
-  {
-    "epwalsh/obsidian.nvim",
-    version = "*", -- recommended, use latest release instead of latest commit
-    lazy = true,
-    ft = "markdown",
-    dependencies = {
-      "nvim-lua/plenary.nvim",
-      "nvim-telescope/telescope.nvim",
-      "nvim-treesitter/nvim-treesitter",
-    },
-    opts = {
-      workspaces = {
-        {
-          name = "black",
-          path = "~/Documents/Black Vault",
-        },
-      },
-    },
-  }
 }
