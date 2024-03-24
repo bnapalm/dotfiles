6c6c264f9694f30ebb39754cefd6c757b9f1dccd
Black Napalm <black@noreply.localhost>
2024-03-24 15:40:41 +0200

    feat(neovide): enable ime

--- private_dot_config/nvim/lua/user/neovide.lua
+++ private_dot_config/nvim/lua/user/neovide.lua
@@ -13,6 +13,8 @@ if vim.g.neovide then
     vim.g.neovide_scale_factor = 1.0
   end)
 
+  vim.g.neovide_input_ime = true
+
   -- mac
   vim.g.neovide_input_use_logo = 1 -- enable use of the logo (cmd) key
   vim.g.neovide_input_macos_alt_is_meta = 1
