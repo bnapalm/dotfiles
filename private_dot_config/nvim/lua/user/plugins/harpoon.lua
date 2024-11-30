ff66a604f5cebab7fdfa7b223c7136a52a925bad
Black Napalm <black@noreply.localhost>
2024-11-30 14:17:55 +0200

    feat(harpoon): update harpoon2

--- private_dot_config/nvim/lua/user/plugins/harpoon.lua
+++ private_dot_config/nvim/lua/user/plugins/harpoon.lua
@@ -1,47 +1,52 @@
 return {
-  "theprimeagen/harpoon",
+  "ThePrimeagen/harpoon",
+  branch = "harpoon2",
+  config = true,
+
   keys = {
     {
       "<leader>a",
       function()
-        require("harpoon.mark").add_file()
+        require("harpoon"):list():add()
       end,
       desc = "Add file to Harpoon",
     },
     {
       "<C-e>",
       function()
-        require("harpoon.ui").toggle_quick_menu()
+        require("harpoon").ui:toggle_quick_menu(require("harpoon"):list())
       end,
       desc = "Toggle Harpoon menu",
     },
     {
       "<C-h>",
       function()
-        require("harpoon.ui").nav_file(1)
+        require("harpoon"):list():select(1)
       end,
       desc = "Harpoon to file 1",
     },
     {
       "<C-t>",
       function()
-        require("harpoon.ui").nav_file(2)
+        require("harpoon"):list():select(2)
       end,
       desc = "Harpoon to file 2",
     },
     {
       "<C-s>",
       function()
-        require("harpoon.ui").nav_file(3)
+        require("harpoon"):list():select(3)
       end,
       desc = "Harpoon to file 3",
     },
     {
       "<C-n>",
       function()
-        require("harpoon.ui").nav_file(4)
+        require("harpoon"):list():select(4)
       end,
       desc = "Harpoon to file 4",
     },
-  }
+  },
+
+  dependencies = { "nvim-lua/plenary.nvim"}
 }
