23a90a482c9ec71ba0f9707d07c1b35dbcf953a2
Black Napalm <black@noreply.localhost>
2024-12-07 00:41:17 +0200

    chore(comment): remove old dep

--- private_dot_config/nvim/lua/user/plugins/init.lua
+++ private_dot_config/nvim/lua/user/plugins/init.lua
@@ -45,14 +45,9 @@ return {
 
   {
     "numToStr/Comment.nvim",
-    config = function()
-      require("Comment").setup({
-        pre_hook = require("ts_context_commentstring.integrations.comment_nvim").create_pre_hook(),
-      })
-    end,
+    config = true,
     dependencies = {
       "nvim-treesitter/nvim-treesitter",
-      'JoosepAlviste/nvim-ts-context-commentstring',
     },
   },
 
