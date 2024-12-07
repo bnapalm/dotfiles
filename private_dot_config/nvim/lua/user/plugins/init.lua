aa78074c2bec312e1a821d23675723c464da60d6
Black Napalm <black@noreply.localhost>
2024-12-08 00:19:05 +0200

    fix(snacks): gitbrowse parser

--- private_dot_config/nvim/lua/user/plugins/init.lua
+++ private_dot_config/nvim/lua/user/plugins/init.lua
@@ -123,7 +123,7 @@ return {
         { "<leader>w",  group = "workspace" },
         -- mini.ai custom objects
         {
-          mode = {"x", "o"},
+          mode = { "x", "o" },
           { "id", desc = "digits" },
           { "ad", desc = "digits" },
           { "ie", desc = "case aware word" },
@@ -181,6 +181,11 @@ return {
       bigfile = { enabled = true },
       bufdelete = { enabled = true },
       dashboard = { enabled = true },
+      gitbrowse = {
+        remote_patterns = {
+          {"^gh%w+:(.+)%.git$", "https://github.com/%1"}
+        },
+      },
       notifier = {
         enabled = true,
         timeout = 3000,
@@ -234,7 +239,7 @@ return {
           Snacks.toggle.diagnostics():map("<leader>ud")
           Snacks.toggle.line_number():map("<leader>ul")
           Snacks.toggle.option("conceallevel", { off = 0, on = vim.o.conceallevel > 0 and vim.o.conceallevel or 2 }):map(
-          "<leader>uc")
+            "<leader>uc")
           Snacks.toggle.treesitter():map("<leader>uT")
           Snacks.toggle.option("background", { off = "light", on = "dark", name = "Dark Background" }):map("<leader>ub")
           Snacks.toggle.inlay_hints():map("<leader>uh")
