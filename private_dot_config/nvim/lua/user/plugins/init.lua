b6dc0bcd623eedfc2d08cd450d408bba10a84038
Black Napalm <black@noreply.localhost>
2024-12-08 17:01:05 +0200

    feat(autopairs): nix semicolon

--- private_dot_config/nvim/lua/user/plugins/init.lua
+++ private_dot_config/nvim/lua/user/plugins/init.lua
@@ -33,15 +33,15 @@ return {
       },
       fast_wrap = {},
     },
-    -- config = function(_, opts)
-    --   require("nvim-autopairs").setup(opts)
-    --   local cmp_autopairs = require("nvim-autopairs.completion.cmp")
-    --   local cmp = require("cmp")
-    --   cmp.event:on(
-    --     "confirm_done",
-    --     cmp_autopairs.on_confirm_done()
-    --   )
-    -- end
+    config = function(_, opts)
+      local npairs = require('nvim-autopairs')
+      local Rule = require('nvim-autopairs.rule')
+
+      npairs.setup(opts)
+      npairs.add_rule(
+        Rule('{', '};', 'nix')
+      )
+    end
   },
 
   {
