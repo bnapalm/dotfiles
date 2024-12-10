bd8e2d4787ed7842e7ffc681563cae448cff7a53
Black Napalm <black@noreply.localhost>
2024-12-10 23:35:15 +0200

    feat(snacks): dashboard custom setup

--- private_dot_config/nvim/lua/user/plugins/folke.lua
+++ private_dot_config/nvim/lua/user/plugins/folke.lua
@@ -123,7 +123,29 @@ return {
     opts = {
       bigfile = { enabled = true },
       bufdelete = { enabled = true },
-      dashboard = { enabled = true },
+      dashboard = {
+        enabled = true,
+        preset = {
+          keys = {
+            { icon = " ", key = "f", desc = "Find File", action = ":lua Snacks.dashboard.pick('files')" },
+            { icon = " ", key = "n", desc = "New File", action = ":ene | startinsert" },
+            { icon = " ", key = "g", desc = "Find Text", action = ":lua Snacks.dashboard.pick('live_grep')" },
+            { icon = " ", key = "r", desc = "Recent Files", action = ":lua Snacks.dashboard.pick('oldfiles')" },
+            {
+              icon = " ",
+              key = "c",
+              desc = "Config",
+              action = function()
+                require("workspaces").open("nvim")
+              end
+            },
+            { icon = "󱉥 ", key = "w", desc = "Workspaces", action = function() require('telescope').extensions.workspaces.workspaces() end },
+            { icon = " ", key = "s", desc = "Restore Session", section = "session" },
+            { icon = "󰒲 ", key = "L", desc = "Lazy", action = ":Lazy", enabled = package.loaded.lazy ~= nil },
+            { icon = " ", key = "q", desc = "Quit", action = ":qa" },
+          },
+        },
+      },
       gitbrowse = {
         remote_patterns = {
           -- ghcustom:foo/bar.git
